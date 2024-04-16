acd0ecdc25859e10e3195842b71722fbb51bcfbf
priyanshualgo <priyanshuknp444@gmail.com>
2024-04-16 22:56:49 +0530

    added signin route

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,74 @@
+import {NextAuthOptions} from "next-auth" 
+
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import bcrypt from "bcryptjs"
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/User";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials : any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user = await User.findOne({
+                        $or: [
+                            { email: credentials.identifier },
+                            { username: credentials.identifier}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error("User not found with this email")
+                    }
+                    if(!user.isVerified){
+                        throw new Error("Verify your account before login!")
+                    }
+                    const isPasswordMatch = await bcrypt.compare(credentials.password, user.password)
+
+                    if(isPasswordMatch){
+                        return user;
+                    }else{
+                        throw new Error("Invalid credentials")
+                    }
+                } catch (error: any) {
+                    throw new Error(error)
+                }
+            }
+        })
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            if(token){
+                session.user._id = token._id
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessage = token.isAcceptingMessage
+                session.user.username = token.username
+            }
+            return session
+        },
+        async jwt({ user , token }) {
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessage = user.isAcceptingMessage
+                token.username = user.username
+            }
+            return token
+        }
+    },
+    pages: {
+        signIn: "/signin"
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
